62468402ad4b500a4d73df7e85df02a0d4539ef8
skuzow <alex@skuzow.net>
2023-02-15 11:41:13 +0100

    feat: init back spring

---
+++ back/src/main/java/net/daw/alist/AlistApplication.java
@@ -0,0 +1,13 @@
+package net.daw.alist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlistApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/net/daw/alist/AlistApplicationTests.java
@@ -0,0 +1,13 @@
+package net.daw.alist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

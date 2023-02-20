be1271862dbf3954aa8085c391083cba34d7a911
skuzow <alex@skuzow.net>
2023-02-20 13:19:07 +0100

    feat: spring hello world

---
+++ back/src/main/java/net/daw/alist/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package net.daw.alist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello(Model model) {
+    model.addAttribute("info", "world");
+    return "hello";
+  }
+
+}

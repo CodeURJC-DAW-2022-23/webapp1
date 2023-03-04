3a3008bce0ec15299d743cb93778b49a0b27262d
skuzow <alex@skuzow.net>
2023-03-05 00:58:33 +0100

    feat: own & external profile

--- back/src/main/java/net/daw/alist/controllers/ProfileController.java
+++ back/src/main/java/net/daw/alist/controllers/ProfileController.java
@@ -4,6 +4,7 @@ import net.daw.alist.models.User;
 import net.daw.alist.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -20,14 +22,31 @@ public class ProfileController {
   UserRepository userRepository;
 
   @GetMapping("/user/{username}")
-  public String profile(Model model, HttpServletResponse httpResponse, @PathVariable String username) throws IOException {
-    Optional<User> user = userRepository.findByUsername(username);
-    if (user.isPresent()) {
-      model.addAttribute("user", user.get());
+  public String profile(
+    Model model,
+    HttpServletResponse httpResponse,
+    Authentication authentication,
+    @PathVariable String username
+  ) throws IOException {
+    Optional<User> optionalUser = userRepository.findByUsername(username);
+    if (optionalUser.isPresent()) {
+      if (isLoggedUser(username, authentication)) {
+        model.addAttribute("ownProfile", true);
+      } else {
+        model.addAttribute("ownProfile", false);
+      }
+      User user = optionalUser.get();
+      model.addAttribute("user", user);
       return "profile";
     }
     httpResponse.sendRedirect("/error");
     return "error";
+  }
+
+  private boolean isLoggedUser(String username, Authentication authentication) {
+    if (authentication == null) return false;
+    User userSession = (User) authentication.getPrincipal();
+    return Objects.equals(username, userSession.getUsername());
   }
 
 }

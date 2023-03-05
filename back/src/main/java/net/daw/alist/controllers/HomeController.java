fc3f8de490e9f8cd42c54508bd7bdd83291c8118
gutche <gledrianregalagutierrez@gmail.com>
2023-03-05 18:49:28 +0100

    feat: jquery.Ajax and spinner

--- back/src/main/java/net/daw/alist/controllers/HomeController.java
+++ back/src/main/java/net/daw/alist/controllers/HomeController.java
@@ -3,14 +3,11 @@ package net.daw.alist.controllers;
 import net.daw.alist.models.Post;
 import net.daw.alist.repositories.PostRepository;
 
-import java.util.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.data.domain.Page;
 
 @Controller
@@ -19,25 +16,13 @@ public class HomeController {
   @Autowired
   private PostRepository postRepository;
 
- //private Map<String, Page<Post>> map = new HashMap<>();
-
   @GetMapping("/{page}")
-  public String home(Model model, @PathVariable int page) {
+  public String home(Model model) {
 
-    Page<Post> posts = postRepository.findAll(PageRequest.of(page, 2));
+    Page<Post> posts = postRepository.findAll(PageRequest.of(0, 10));
     model.addAttribute("posts", posts);
-/*     Page<Post> newPage = getNextPage(0);
-    map.put("posts", newPage);
-    model.mergeAttributes(map); */
-    
+
     return "home";
   }
-
-/*   private Page<Post> getNextPage(int page) {
-    return postRepository.findAll(PageRequest.of(page, 2));
-  } */
-
-
-
 
 }
--- back/src/main/resources/static/scripts/loadOnScroll.js
+++ back/src/main/resources/static/scripts/loadOnScroll.js
@@ -1,12 +1,7 @@
-let page = 0;
-
-window.addEventListener('scroll', () => {
-  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-  if (scrollTop + clientHeight >= scrollHeight) {
-    page++;
-    async () => await fetch('http://localhost:8443/page');
-
-    console.log('User wants more data');
-    console.log(page);
+$(window).scroll(function () {
+  if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+    $('.spinner-border').removeClass('invisible').addClass('visible');
+    alert('bottom!');
+    // getData();
   }
 });

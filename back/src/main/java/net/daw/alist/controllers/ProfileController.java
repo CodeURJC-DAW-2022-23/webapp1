294af310dfe999fde3f202eaf93b16d58461b637
skuzow <alex@skuzow.net>
2023-03-08 19:01:25 +0100

    feat: profile follow

--- back/src/main/java/net/daw/alist/controllers/ProfileController.java
+++ back/src/main/java/net/daw/alist/controllers/ProfileController.java
@@ -58,6 +58,22 @@ public class ProfileController {
     return "profile";
   }
 
+  @GetMapping("/user/{username}/following")
+  public String following(Model model, @PathVariable String username) {
+    model.addAttribute("follow", "Following");
+    model.addAttribute("user", userProfile);
+    model.addAttribute("followList", userProfile.getFollowing());
+    return "follow";
+  }
+
+  @GetMapping("/user/{username}/followers")
+  public String followers(Model model, @PathVariable String username) {
+    model.addAttribute("follow", "Followers");
+    model.addAttribute("user", userProfile);
+    model.addAttribute("followList", userProfile.getFollowers());
+    return "follow";
+  }
+
   @GetMapping("/user/{username}/follow")
   public String follow(@PathVariable String username) {
     userSessionRepo.follow(userProfile);

5c4e5c0e05db591751e8c4faac91c56fdeef4ea5
franchescoURJC <f.elzaurdia.2020@alumnos.urjc.es>
2023-03-09 22:38:45 +0100

    feat: mobile icons highlight and navigation improvements, followers page searchbar z-index fix, mobile navbar open/close animation improved.

--- back/src/main/resources/static/scripts/navbar.js
+++ back/src/main/resources/static/scripts/navbar.js
@@ -1,13 +1,20 @@
 
 var url = window.location.href;
 var signOutButton = document.getElementById("sign-out");
+const profile = document.getElementsByClassName("profileNavIcon");
+const heart = document.getElementsByClassName("heartNavIcon");
+const compass = document.getElementsByClassName("compassNavIcon");
+
 
 if(url.includes("/user/") && (signOutButton != null && signOutButton.value =='')){
-  document.getElementById("profileNavIcon").innerHTML = "<i class=\"fa-solid fa-user fa-xl\"></i>";
+  profile[0].innerHTML = "<i class=\"fa-solid fa-user fa-xl\"></i>";
+  profile[1].innerHTML = "<i class=\"fa-solid fa-user fa-xl\"></i>";
 } else if(url.includes("/followed-users/")){
-  document.getElementById("heartNavIcon").innerHTML = "<i class=\"fa-solid fa-heart fa-xl\"></i>";
+  heart[0].innerHTML = "<i class=\"fa-solid fa-heart fa-xl\"></i>";
+  heart[1].innerHTML = "<i class=\"fa-solid fa-heart fa-xl\"></i>";
 } else if(url.includes("/admin-panel") || url.includes("/top-list/") || url.includes("/user/")){
 } else{
-  document.getElementById("compassNavIcon").innerHTML = "<i class=\"fa-solid fa-compass fa-xl\"></i>";
+  compass[0].innerHTML = "<i class=\"fa-solid fa-compass fa-xl\"></i>";
+  compass[1].innerHTML = "<i class=\"fa-solid fa-compass fa-xl\"></i>";
 }
 

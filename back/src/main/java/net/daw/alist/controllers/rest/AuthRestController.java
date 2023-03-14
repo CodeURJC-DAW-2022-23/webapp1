7f02fca324dd3627b6c88a3649d0e48c7bbb0245
franchescoURJC <f.elzaurdia.2020@alumnos.urjc.es>
2023-03-14 13:47:35 +0100

    feat: AuthRestController implemented

--- back/src/main/java/net/daw/alist/controllers/rest/AuthRestController.java
+++ back/src/main/java/net/daw/alist/controllers/rest/AuthRestController.java
@@ -1,18 +1,40 @@
 package net.daw.alist.controllers.rest;
 
+import net.daw.alist.security.jwt.AuthResponse;
+import net.daw.alist.security.jwt.LoginRequest;
+import net.daw.alist.security.jwt.UserLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestController {
 
-  @GetMapping("/login")
-  public ResponseEntity<String> login() {
-    return new ResponseEntity<>(HttpStatus.OK);
+  @Autowired
+  private UserLoginService userService;
+
+  @PostMapping("/sign-in")
+  public ResponseEntity<AuthResponse> login(
+          @CookieValue(name = "accessToken", required = false) String accessToken,
+          @CookieValue(name = "refreshToken", required = false) String refreshToken,
+          @RequestBody LoginRequest loginRequest) {
+    return userService.login(loginRequest, accessToken, refreshToken);
+  }
+
+  @PostMapping("/refresh")
+  public ResponseEntity<AuthResponse> refreshToken(
+          @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+    return userService.refresh(refreshToken);
+  }
+
+  @PostMapping("/sign-out")
+  public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
+    return ResponseEntity.ok(new AuthResponse(AuthResponse.Status.SUCCESS, userService.logout(request, response)));
   }
 
 }

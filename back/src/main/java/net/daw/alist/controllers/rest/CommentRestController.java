6ef900c8fae60e2445fc11b84f0e09f169054e71
franchescoURJC <f.elzaurdia.2020@alumnos.urjc.es>
2023-03-14 13:44:55 +0100

    refactor: application.properties and rest api urls

--- back/src/main/java/net/daw/alist/controllers/rest/CommentRestController.java
+++ back/src/main/java/net/daw/alist/controllers/rest/CommentRestController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/comment")
+@RequestMapping("/api/comments")
 public class CommentRestController {
 }
--- back/src/main/java/net/daw/alist/controllers/rest/UserRestController.java
+++ back/src/main/java/net/daw/alist/controllers/rest/UserRestController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserRestController {
 }

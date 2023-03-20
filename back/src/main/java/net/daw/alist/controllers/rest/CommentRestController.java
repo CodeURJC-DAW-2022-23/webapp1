3a7943cb1bf0eb3d5327cf6bc977a70b7b26c7d2
Vicente <v.gonzalez.2020@alumnos.urjc.es>
2023-03-20 18:39:26 +0100

    feat: methods to get post comments and user comments implemented in CommentRestController

--- back/src/main/java/net/daw/alist/controllers/rest/CommentRestController.java
+++ back/src/main/java/net/daw/alist/controllers/rest/CommentRestController.java
@@ -2,12 +2,15 @@ package net.daw.alist.controllers.rest;
 
 import net.daw.alist.models.Comment;
 import net.daw.alist.models.Post;
+import net.daw.alist.models.User;
 import net.daw.alist.services.PostService;
+import net.daw.alist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,6 +18,9 @@ import java.util.Optional;
 public class CommentRestController {
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/{postId}")
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,16 +35,24 @@ public class CommentRestController {
         return null;
     }
 
-    @GetMapping("/{postId}/{commentId}")
-    public ResponseEntity<Comment> getComment(@PathVariable long postId, @PathVariable long commentId) {
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<Comment>> getPostComments(@PathVariable long postId) {
         Optional<Post> optionalPost = postService.findByID(postId);
         if (optionalPost.isPresent()) {
             Post post = optionalPost.get();
-            Optional<Comment> optionalComment = post.getCommentByID(commentId);
-            if(optionalComment.isPresent()){
-                Comment comment = optionalComment.get();
-                return new ResponseEntity<>(comment, HttpStatus.OK);
-            }
+            List<Comment> comments = post.getComments();
+            return new ResponseEntity<>(comments, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Comment>> getUserComments(@PathVariable long userId) {
+        Optional<User> optionalUser = userService.findByID(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            List<Comment> comments = user.getComments();
+            return new ResponseEntity<>(comments, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }

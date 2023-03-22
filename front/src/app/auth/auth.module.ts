82249e7b6607e5305b2c9a02ecdd1d15d583dde0
skuzow <alex@skuzow.net>
2023-03-22 18:32:23 +0100

    feat: register reactive form

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [SignInComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   exports: [SignInComponent, RegisterComponent],
 })
 export class AuthModule {}

e2e67fd102d9bc8565fe74f522d40cf50264c326
skuzow <alex@skuzow.net>
2023-03-22 19:09:34 +0100

    feat: sign in reactive form

--- front/src/app/auth/pages/sign-in/sign-in.component.ts
+++ front/src/app/auth/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['../form.css'],
 })
-export class SignInComponent {}
+export class SignInComponent {
+  signInForm: FormGroup = this.fb.group({
+    username: [, [Validators.required, Validators.minLength(3)]],
+    password: [, [Validators.required, Validators.minLength(5)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isInvalidField(field: string | string[]) {
+    return (
+      this.signInForm.get(field)?.touched && this.signInForm.get(field)?.errors
+    );
+  }
+
+  save() {
+    // TODO: use service for checking
+    console.log(this.signInForm.value);
+    this.signInForm.reset();
+  }
+}

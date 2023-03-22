adb1a68c9f3a4692436aad2806ded06e137a9dde
skuzow <alex@skuzow.net>
2023-03-22 12:19:28 +0100

    feat: error page

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NavComponent } from './components/nav/nav.component';
 import { SearchComponent } from './components/search/search.component';
@@ -7,7 +8,7 @@ import { ErrorComponent } from './pages/error/error.component';
 
 @NgModule({
   declarations: [NavComponent, SearchComponent, ErrorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavComponent, ErrorComponent],
 })
 export class SharedModule {}

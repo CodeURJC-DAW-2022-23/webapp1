358a574d31030945c3d3b3236e9ca7f7c97777bd
gutche <gledrianregalagutierrez@gmail.com>
2023-03-23 21:31:37 +0100

    docs: AjaxRestController

--- back/src/main/java/net/daw/alist/controllers/rest/AjaxRestController.java
+++ back/src/main/java/net/daw/alist/controllers/rest/AjaxRestController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import net.daw.alist.models.Post;
 import net.daw.alist.models.User;
 import net.daw.alist.services.PostService;
@@ -19,13 +25,25 @@ public class AjaxRestController {
   @Autowired
   PostService postService;
 
+  @Operation(summary = "Load more posts")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "New posts loaded", content = {
+                  @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Post.class)))}),
+          @ApiResponse(responseCode = "404", description = "No new posts found", content = @Content)
+  })
   @GetMapping("/posts")
   public Page<Post> getNewPosts(@RequestParam int page) {
-    if (page <= (int) Math.ceil(postService.count()/2))
+    if (page <= (int) Math.ceil(postService.count() / 2))
       return postService.getPosts(page);
     return null;
   }
 
+  @Operation(summary = "Load more posts from followed-users")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "New posts loaded", content = {
+                  @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Post.class)))}),
+          @ApiResponse(responseCode = "404", description = "No new posts found", content = @Content)
+  })
   @GetMapping("/followed-users/posts")
   public Page<Post> getFollowedUsersPosts(Authentication authentication, @RequestParam int page) {
     User currentUser = (User) authentication.getPrincipal();

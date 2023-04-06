a257b12a5deb6f7f18b568eb5150bd1cc0f5a89b
gutche <gledrianregalagutierrez@gmail.com>
2023-04-06 18:43:56 +0200

    feat: icon glow based on route

--- front/src/app/shared/components/nav/nav.component.ts
+++ front/src/app/shared/components/nav/nav.component.ts
@@ -1,8 +1,53 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {}
+export class NavComponent {
+  compassIconGlow: boolean;
+  heartIconGlow: boolean;
+  profileIconGlow: boolean;
+  currentRoute: string;
+  filtered: boolean;
+
+  constructor(private router: Router) {
+    this.filtered = false;
+    this.currentRoute = '';
+    this.compassIconGlow = false;
+    this.heartIconGlow = false;
+    this.profileIconGlow = false;
+    this.router.events
+      .pipe(
+        filter((e: Event): e is NavigationEnd => e instanceof NavigationEnd)
+      )
+      .subscribe(e => {
+        this.currentRoute = e.url;
+        this.toggleOff();
+        if (this.currentRoute === '/profile') {
+          this.profileIconGlow = true;
+        } else if (this.currentRoute === '/explore') {
+          this.compassIconGlow = true;
+        }
+      });
+  }
+
+  toggleHeartIcon = () => {
+    this.toggleOff();
+    this.filtered = true;
+    this.heartIconGlow = true;
+  };
+
+  toggleOff = () => {
+    this.compassIconGlow = false;
+    this.heartIconGlow = false;
+    this.profileIconGlow = false;
+    this.heartIconGlow = false;
+    this.filtered = false;
+  };
+
+  ngOnInit() {}
+}

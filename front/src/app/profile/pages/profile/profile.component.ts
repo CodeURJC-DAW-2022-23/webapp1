58dd3b76fe091276e315cad5942b4eb65e9a798c
skuzow <alex@skuzow.net>
2023-04-14 20:52:58 +0200

    feat: profile basis

--- front/src/app/profile/pages/profile/profile.component.ts
+++ front/src/app/profile/pages/profile/profile.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  user: any;
+  notGuest: boolean = false;
+  ownProfile: boolean = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const username: string = this._getUsernameFormURL(this.router.url);
+    this._checkLoggedUser(username);
+    this.userService.getUser(username).subscribe({
+      next: user => (this.user = user),
+      error: _ => this.router.navigate(['/error']),
+    });
+  }
+
+  private _getUsernameFormURL(url: string): string {
+    return url.split('/')[2];
+  }
+
+  private _checkLoggedUser(username: string) {
+    if (!this.authService.isLoggedIn()) return;
+    this.notGuest = true;
+    const loggedUser = this.authService.loggedUser;
+    if (loggedUser!.username === username) this.ownProfile = true;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}

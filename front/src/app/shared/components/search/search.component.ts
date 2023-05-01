f89bf2c933ec96e5bd1a652c9037e38c9358f642
Alberto Dekenó <102819481+Kr4ll@users.noreply.github.com>
2023-05-01 10:58:32 +0200

    feat:search bar initializing

--- front/src/app/shared/components/search/search.component.ts
+++ front/src/app/shared/components/search/search.component.ts
@@ -1,8 +1,21 @@
+import { SearchService } from './search.service';
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {}
+export class SearchComponent {
+  userResults: User[] = []
+
+  constructor(private searchService: SearchService) { }
+
+  getUser(username: String) {
+    this.searchService.getUser(username).subscribe(
+      Response => this.userResults = Response as User[]
+    )
+  }
+
+}

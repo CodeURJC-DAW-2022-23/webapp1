201efd47ea947d4179d26b5a3d0a4895707b3a30
franchescoURJC <f.elzaurdia.2020@alumnos.urjc.es>
2023-05-07 15:34:34 +0200

    feat: working front in /new

--- back/src/main/java/net/daw/alist/controllers/SPAController.java
+++ back/src/main/java/net/daw/alist/controllers/SPAController.java
@@ -1,7 +1,10 @@
 package net.daw.alist.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class SPAController {
 	@GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
 	public String redirect() {

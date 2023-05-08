c882cfc1170c9f66543256e6e39dd535585ba0bd
Alberto Dekenó <102819481+Kr4ll@users.noreply.github.com>
2023-05-08 07:25:39 +0200

    fix: SPA search bar

--- front/src/app/shared/components/search/search.service.ts
+++ front/src/app/shared/components/search/search.service.ts
@@ -9,16 +9,17 @@ import { Post } from 'src/app/models/post.model';
   providedIn: 'root'
 })
 export class SearchService {
-
+  private apiUrl = '/api/posts/names/';
+  private apiUrlUsers = '/api/users/names/';
 
   constructor(private httpClient: HttpClient) { }
 
   getUser(username: String): Observable<User[]> {
-    return this.httpClient.get<User[]>("api/users/names/" + username);
+    return this.httpClient.get<User[]>(this.apiUrlUsers + username);
   }
 
   getPost(username: String): Observable<Post[]> {
-    return this.httpClient.get<Post[]>("api/posts/names/" + username);
+    return this.httpClient.get<Post[]>(this.apiUrl + username);
   }
 
 
